f1079a30c8ff48fcf53204347873d4775efa5ca2
JHJ <845776779@qq.com>
2019-10-29 22:28:47 +0800

    java入门练习

---
+++ JavaTest.java
@@ -0,0 +1,20 @@
+public class JavaTest{
+	public static void main(String[] args){
+		final double PI = 3.14;   //声明常量
+		//PI = 1.1;  //再次给常量赋值会出错
+		System.out.println("常量PI的值为:" + PI);
+        System.out.println("半径为3的圆的周长为：" + PI*2*3);
+		System.out.println("半径为4的圆的面积为：" + PI*4*4);
+		//int x;      //声明int型x变量
+		//int x,y;    //同时声明int型x，y变量
+		//int x=10,y=-5;  //同时声明int型x，y变量并赋予初值
+		//int x=5+23;
+		int a=15;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);  //输出35
+		
+		
+		
+		}
+}
