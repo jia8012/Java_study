272c528d15a3c17a3f373b79a1757d9c1d21c698
JHJ <845776779@qq.com>
2019-11-01 23:03:27 +0800

    更新代码

--- MethodDemo.java
+++ MethodDemo.java
@@ -56,7 +56,7 @@ public class MethodDemo{
 	*/
 	
 	public static int Fib(int n){
-		if(n == 0 || n ==1){
+		if(n == 1 || n ==2){
 			return 1;
 		}else{
 			return Fib(n-2)+Fib(n-1);
@@ -64,7 +64,7 @@ public class MethodDemo{
 	}	
 	public static void main(String[] args){
 		int n = 10;
-		for(int i = 0; i < n; i++){
+		for(int i = 1; i <= n; i++){
 		System.out.println(Fib(i));
 		}
 	}
