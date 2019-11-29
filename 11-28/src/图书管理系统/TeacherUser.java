ce4f83a645a70746ccb90bdb52838dbc0d4c998a
JHJ <845776779@qq.com>
2019-11-29 23:34:41 +0800

    补充代码 11-28\src\图书管理系统\TeacherUser.java

--- 11-28/src/图书管理系统/TeacherUser.java
+++ 11-28/src/图书管理系统/TeacherUser.java
@@ -1,5 +1,7 @@
 package 图书管理系统;
 
+import java.util.Scanner;
+
 public class TeacherUser extends User {
     @Override
     public void menu() { //菜单
@@ -12,14 +14,47 @@ public class TeacherUser extends User {
     public void operation(int select) {  //菜单的选择
         switch (select) {
             case 1:
-                //addBook();  //上架
+                addBook();  //上架
                 break;
             case 2:
-                //removeBook();  //下架
+                removeBook();  //下架
                 break;
             case 3:
-                //listBook();
+                listBook();
                 break;
         }
+    }
+
+    private void listBook() {
+        BookShelf bookShelf = BookShelf.getInstance();
+        Book[] books = bookShelf.getBook();
+        System.out.println("书名，当前数量，总数");
+        for (Book book : books) {
+            System.out.println(
+                      book.title + ","
+                    + book.currentCount + ","
+                    + book.totalCount);
+        }
+    }
+
+    private void removeBook() {
+
+    }
+
+    private void addBook() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入书名");
+        String title = scanner.nextLine();
+        System.out.println("这次上架多少本");
+        int count = scanner.nextInt();
+
+        Book book = new Book();
+        book.title = title;
+        book.totalCount = count;
+        book.currentCount = count;
+
+        BookShelf bookShelf = BookShelf.getInstance();
+        bookShelf.add(book);
+        System.out.println("上架成功");
     }
 }
