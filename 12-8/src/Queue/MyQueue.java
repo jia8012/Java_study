333a642620b615413601a2cda67212944955a677
JHJ <845776779@qq.com>
2019-12-09 21:02:47 +0800

    学习提交： 1.用队列实现实现栈：12-8\src\Queue\MyQueue.java

---
+++ 12-8/src/Queue/MyQueue.java
@@ -0,0 +1,71 @@
+package Queue;
+
+import java.util.Stack;
+
+//用栈实现队列
+class MyQueue {
+	Stack<Integer> stack;
+	Stack<Integer> stack2;
+
+
+	/** Initialize your data structure here. */
+	public MyQueue() {
+		stack = new Stack<>();
+		stack2 = new Stack<>();
+	}
+
+	/** Push element x to the back of queue. */
+	public void push(int x) {
+		stack.push(x);
+	}
+
+	/** Removes the element from in front of queue and returns that element. */
+	public int pop() {  //删除队列首元素
+		if (stack2.isEmpty()) {
+			int size = stack.size();
+			for (int i = 1; i < size; i++) {
+				stack2.push(stack.pop());
+			}
+		}
+		return stack.pop();
+	}
+
+	/** Get the front element. */
+	public int peek() {  //返回队列首元素
+		if (stack2.isEmpty()) {
+			int size = stack.size();
+			for (int  i = 1; i < size; i++) {
+				stack2.push(stack.pop());
+			}
+		}
+		return stack.peek();
+
+	}
+
+	/** Returns whether the queue is empty. */
+	public boolean empty() {
+		return stack.isEmpty() && stack2.isEmpty();
+
+	}
+
+	public static void main(String[] args) {
+		MyQueue queue = new MyQueue();
+		queue.push(1);
+		queue.push(2);
+		System.out.println(queue.peek());  // 返回 1
+		queue.push(3);
+		System.out.println(queue.peek());  // 返回3
+		System.out.println(queue.pop());   // 返回 1
+		System.out.println(queue.empty()); // 返回 false
+
+	}
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
