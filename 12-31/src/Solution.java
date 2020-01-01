c00765f8a9358f130bca4a25b6aa73694575559f
JHJ <845776779@qq.com>
2020-01-01 20:53:10 +0800

    从前序遍历与中序遍历序列构造二叉树

--- 12-31/src/Solution.java
+++ 12-31/src/Solution.java
@@ -53,4 +53,33 @@ public class Solution {
 			return root;
 		}
 	}
+
+
+
+	//从前序遍历与中序遍历序列构造二叉树
+	TreeNode buildTree(int[] preorder, int[] inorder) {
+		return buildTreeHelper(preorder,  inorder, (long)Integer.MAX_VALUE + 1);
+	}
+	int pre = 0;
+	int in = 0;
+	private TreeNode buildTreeHelper(int[] preorder, int[] inorder, long stop) {
+		//到达末尾返回 null
+		if(pre == preorder.length){
+			return null;
+		}
+		//到达停止点返回 null
+		//当前停止点已经用了，in 后移
+		if (inorder[in] == stop) {
+			in++;
+			return null;
+		}
+		int root_val = preorder[pre++];
+		TreeNode root = new TreeNode(root_val);
+		//左子树的停止点是当前的根节点
+		root.left = buildTreeHelper(preorder,  inorder, root_val);
+		//右子树的停止点是当前树的停止点
+		root.right = buildTreeHelper(preorder, inorder, stop);
+		return root;
+	}
+
 }
