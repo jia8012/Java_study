ff90569b07353d07c3f81934fc66bc7fd9f437fc
JHJ <845776779@qq.com>
2020-01-02 19:25:10 +0800

    数组移除元素

--- 12-31/src/Solution.java
+++ 12-31/src/Solution.java
@@ -82,4 +82,31 @@ public class Solution {
 		return root;
 	}
 
+
+	//数组移除元素   ①
+	public int removeElement(int[] nums, int val) {
+		int i = 0;
+		for (int j = 0; j < nums.length; j++) {
+			if (nums[j] != val) {
+				nums[i] = nums[j];
+				i++;
+			}
+		}
+		return i;
+	}
+	//数组移除元素   ②
+	public int RemoveElement(int[] nums, int val) {
+		int i = 0;
+		int n = nums.length;
+		while (i < n) {
+			if (nums[i] == val) {
+				nums[i] = val;
+				n--;
+			}else {
+				i++;
+			}
+		}
+		return n;
+	}
+
 }
