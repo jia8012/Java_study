28887f4f88e3e63996198039248173ffe0965a85
JHJ <845776779@qq.com>
2020-01-18 20:13:34 +0800

    赎金信、回文数

--- 2020-1-17/src/Solution.java
+++ 2020-1-17/src/Solution.java
@@ -1,6 +1,7 @@
 public class Solution {
 	public  boolean canConstruct(String ransomNote, String magazine) {
 
+		//赎金信
 		if (magazine.length() < ransomNote.length()) {
 			return false;
 		}
@@ -13,5 +14,20 @@ public class Solution {
 			caps[c - 97] = index + 1;
 		}
 		return true;
+
+
+ 	}
+
+	//回文数
+	public boolean isPalindrome(int x) {
+		if (x < 0 || x % 10 == 0 && x!= 0) {
+			return false;
+		}
+		int revertedNumber = 0;
+		while (x > revertedNumber) {
+			revertedNumber = revertedNumber * 10 + x % 10;
+			x /= 10;
+		}
+		return x == revertedNumber || x == revertedNumber / 10;
 	}
 }
