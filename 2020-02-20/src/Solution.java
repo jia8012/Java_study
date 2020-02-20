d2df3a4659395e3e7d3326bc9752b07609db24c8
JHJ <845776779@qq.com>
2020-02-20 23:07:41 +0800

    LeetCode练习

--- 2020-02-20/src/Solution.java
+++ 2020-02-20/src/Solution.java
@@ -6,7 +6,7 @@ class Solution {
 			int dif = target - nums[i];
 			// j = i + 1 的目的是减少重复计算和避免两个元素下标相同
 			for (int j = i + 1; j < nums.length; j++) {
-				if (nums[j] == dif){
+				if (nums[j] == dif) {
 					res[0] = i;
 					res[1] = j;
 					return res;
@@ -20,7 +20,7 @@ class Solution {
 	public String addBinary(String a, String b) {
 		StringBuilder ans = new StringBuilder();
 		int ca = 0;
-		for(int i = a.length() - 1, j = b.length() - 1;i >= 0 || j >= 0; i--, j--) {
+		for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
 			int sum = ca;
 			sum += i >= 0 ? a.charAt(i) - '0' : 0;
 			sum += j >= 0 ? b.charAt(j) - '0' : 0;
@@ -69,7 +69,7 @@ class Solution {
 	//在排序数组中查找元素的第一个和最后一个位置
 	public int[] searchRange(int[] nums, int target) {
 		int[] targetRange = {-1, -1};
-		
+
 		for (int i = 0; i < nums.length; i++) {
 			if (nums[i] == target) {
 				targetRange[0] = i;
@@ -81,17 +81,45 @@ class Solution {
 			return targetRange;
 		}
 
-		for (int j = nums.length-1; j >= 0; j--) {
+		for (int j = nums.length - 1; j >= 0; j--) {
 			if (nums[j] == target) {
 				targetRange[1] = j;
 				break;
 			}
 		}
-
 		return targetRange;
 	}
 
+	//判断回文字符串，不论大小写和符号
+	public boolean isPalindrome(String s) {
+		int i = 0, j = s.length() - 1;
+		while (i < j) {
+			while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+			while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+			if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
+			i++;
+			j--;
+		}
+		return true;
+	}
 
-
-
+	//压缩字符串
+	public int compress(char[] chars) {     // 数组大小范围： 1 <= chars.length <= 1000
+		int left = 0;
+		int size = 0;
+		// 由于最后一个字符也需要判断，所以将右指针终点放到数组之外一格
+		for (int right = 0; right <= chars.length; right++) {
+			// 当遍历完成，或右指针元素不等于左指针元素时，更新数组
+			if (right == chars.length || chars[right] != chars[left]) {
+				chars[size++] = chars[left];    // 更新字符
+				if (right - left > 1) {         // 更新计数，当个数大于 1 时才更新
+					for(char c : String.valueOf(right - left).toCharArray()) {
+						chars[size++] = c;
+					}
+				}
+				left = right;
+			}
+		}
+		return size;
+	}
 }
